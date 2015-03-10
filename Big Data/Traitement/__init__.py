1c461cdf5235a54bce1f09f8d054fa0156917e05
Aimainaims <serre@et.esiea.fr>
2015-03-10 09:37:03 +0100

    Implémentation Scikit-learn + camembert matplotlib

--- Big Data/Traitement/__init__.py
+++ Big Data/Traitement/__init__.py
@@ -6,6 +6,8 @@ import pickle
 import math
 from cmath import log
 import copy
+import matplotlib
+import matplotlib.pyplot as plt
 
 MONGO_DATABASE_NAME = 'twitter_db3'
 MONGO_COLLECTION_NAME = 'twitter_collection3'
@@ -183,7 +185,7 @@ collection = db[MONGO_COLLECTION_NAME]
 nombredocs = collection.find().count()
 a = dict()
 a = LoadDictionnary(collection)
-output = open('/home/alexis/Bureau/Text.file','wb')
+output = open('/Users/K2/Desktop/Text.file','wb')
 
 occurence = list(a.values())    #nombre
 words = list(a.keys())          #mots
@@ -217,3 +219,23 @@ print hashtagoccu
 #print percentage(hashtagoccu)
 position = interestingPos(occurence, moyenne(occurence))
 #print interestingWords(words, position, 5)
+
+
+name = [words[0], words[1], words[2], words[3], words[4], words[5]]
+data = [occurence[0], occurence[1], occurence[2], occurence[3], occurence[4], occurence[5]]
+
+plt.figure(1)
+plt.subplot(211)
+explode=(0, 0, 0, 0, 0, 0)
+plt.pie(data, explode=explode, labels=name, autopct='%1.1f%%', shadow=True)
+plt.axis('equal')
+
+name2 = [hashtaglist[0], hashtaglist[1], hashtaglist[2], hashtaglist[3], hashtaglist[4], hashtaglist[5]]
+data2 = [hashtagoccu[0], hashtagoccu[1], hashtagoccu[2], hashtagoccu[3], hashtagoccu[4], hashtagoccu[5]]
+
+plt.subplot(212)
+plt.pie(data2, explode=explode, labels=name2, autopct='%1.1f%%', shadow=True)
+plt.axis('equal')
+plt.show()
+
+
